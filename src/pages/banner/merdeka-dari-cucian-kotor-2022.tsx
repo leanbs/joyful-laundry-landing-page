f8975c11eb1fd812d69162ecce7d86e17bc7efb0
leanbs <davin.yu1110@gmail.com>
2022-08-10 17:31:47 +0700

    feat(banner): independence day banner

--- src/pages/banner/merdeka-dari-cucian-kotor-2022.tsx
+++ src/pages/banner/merdeka-dari-cucian-kotor-2022.tsx
@@ -1,10 +1,15 @@
+import Image from 'next/image';
+import download from 'downloadjs';
+
 const IndependenceDay = () => {
   const handleShare = async () => {
-    const response = await fetch('https://joyful.id/joyful-logo.png');
+    const response = await fetch(
+      'https://joyful.id/image/joyful-laundry-merdeka.jpg',
+    );
     const blob = await response.blob();
     const filesArray: File[] = [
-      new File([blob], 'joyful-merdeka-2022.png', {
-        type: 'image/png',
+      new File([blob], 'joyful-laundry-merdeka-2022.jpg', {
+        type: 'image/jpeg',
         lastModified: new Date().getTime(),
       }),
     ];
@@ -16,12 +21,39 @@ const IndependenceDay = () => {
     });
   };
 
+  const handleDownload = () => {
+    download(
+      'https://joyful.id/image/joyful-laundry-merdeka.jpg',
+      `https://joyful.id/image/joyful-laundry-merdeka.jpg`,
+    );
+  };
+
   return (
-    <div>
-      <img src="/joyful-logo.png" className="w-20 h-10 object-contain" />
-      <button className="bg-orange-600 p-4 rounded-lg" onClick={handleShare}>
-        share dong
-      </button>
+    <div className="text-center mb-4">
+      <div className="p-4">
+        <Image
+          alt="joyful-banner-merdeka-2022"
+          src="/image/joyful-laundry-merdeka.jpg"
+          className="object-contain"
+          layout="responsive"
+          width={1200}
+          height={1200}
+        />
+      </div>
+      <div className="flex gap-x-4 items-center justify-center">
+        <button
+          className="bg-orange-600 text-white text-bold px-4 py-2 rounded-lg"
+          onClick={handleShare}
+        >
+          Bagikan
+        </button>
+        <button
+          className="bg-orange-600 text-white text-bold px-4 py-2 rounded-lg"
+          onClick={handleDownload}
+        >
+          Download
+        </button>
+      </div>
     </div>
   );
 };
