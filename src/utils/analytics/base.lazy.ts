20798eef7ac183878b3608f991d6e0cb7b409ba4
leanbs <davin.yu1110@gmail.com>
2022-10-21 14:10:01 +0700

    fix types

--- src/utils/analytics/base.lazy.ts
+++ src/utils/analytics/base.lazy.ts
@@ -1,14 +1,16 @@
 interface AnalyticsModule {
   init: () => void;
-  gaAll: Function;
+  gaAll: any;
 }
 
 let analyticsModule: AnalyticsModule;
 
 export const baseAnalytics = async () => {
   if (!analyticsModule) {
-    analyticsModule = await import(/* webpackChunkName: "analytics" */'./base.js');
+    analyticsModule = await import(
+      /* webpackChunkName: "analytics" */ './base.js'
+    );
   }
 
   return analyticsModule;
-}; 
+};
